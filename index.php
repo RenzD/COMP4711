12376b5f36b3203ca9113684f79cccdd3bca72b1
RenzD <RenzD@192.168.0.110>
2017-01-15 16:24:09 -0800

    Setup

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo 'hello out there...i live';
+        ?>
+    </body>
+</html>
